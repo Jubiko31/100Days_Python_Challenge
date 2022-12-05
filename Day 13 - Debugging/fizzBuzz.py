3e4c9858b1f44d699e3a1abee87eb8755b6c2577
Jubiko31 <jubiko.macaberidze1@gmail.com>
2022-12-05 11:32:54 +0400

    #day13 - new: debugging

---
+++ Day 13 - Debugging/fizzBuzz.py
@@ -0,0 +1,22 @@
+# Assignment to Debug:
+# for number in range(1, 101):
+#     if number % 3 == 0 or number % 5 == 0:
+#         print("FizzBuzz")
+#     if number % 3 == 0:
+#         print("Fizz")
+#     if number % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print([number])
+
+# Correct version:
+for number in range(1, 101):
+    print(f"Currently checking number {number}")
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
