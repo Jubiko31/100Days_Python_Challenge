8b81dacc802250641204804b3d9970844aadd1f3
Jubiko31 <jubiko.macaberidze1@gmail.com>
2022-12-12 23:11:10 +0400

    #day22 - dev: turtle module, game logic, classes, OOP

---
+++ Day 22 - Classic_Pong_Game/main.py
@@ -0,0 +1,45 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+from time import sleep
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+score = Scoreboard()
+
+screen.listen()
+screen.onkey(r_paddle.up, "Up")
+screen.onkey(r_paddle.down, "Down")
+screen.onkey(l_paddle.up, "w")
+screen.onkey(l_paddle.down, "s")
+
+game_on = True
+while game_on:
+    
+    sleep(0.1)
+    screen.update()
+    ball.move()
+    print(ball.move_speed)
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+        
+    if ball.xcor() > 380:
+        ball.reset_pos()
+        score.l_point()
+
+    if ball.xcor() < -380:
+        ball.reset_pos()
+        score.r_point()
+
+screen.exitonclick()
