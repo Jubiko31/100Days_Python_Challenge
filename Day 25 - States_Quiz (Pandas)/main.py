097d8c4a454a8185088a56850638edb559997efb
Jubiko31 <jubiko.macaberidze1@gmail.com>
2022-12-19 16:42:36 +0400

    #day25 - new: pandas library, dev: csv files

---
+++ Day 25 - States_Quiz (Pandas)/main.py
@@ -0,0 +1,34 @@
+import pandas
+import turtle
+
+BG_IMAGE = "states_img.gif"
+
+data = pandas.read_csv('50_states.csv')
+states = data.state.tolist()
+
+screen = turtle.Screen()
+screen.title("U.S. States Quiz Game")
+screen.addshape(BG_IMAGE)
+turtle.shape(BG_IMAGE)
+
+guessed_state = []
+
+while len(guessed_state) < 50:
+    input_field = screen.textinput(title=f"{len(guessed_state)}/50 states correct", prompt="Enter state name")
+    answer = input_field.title()
+    if answer == 'Exit':
+        missed_states = []
+        for state in states:
+            if state not in guessed_state:
+                missed_states.append(state)
+        res = pandas.DataFrame(missed_states, columns=["Missed States"])
+        res.to_csv('results.csv')
+        break
+    if answer in states:
+        guessed_state.append(answer)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(state_data.state.item())
