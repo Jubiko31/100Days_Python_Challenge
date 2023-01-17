ff0949ebbba70a07ef945f029769c80f749ec886
Jubiko31 <jubiko.macaberidze1@gmail.com>
2023-01-18 00:11:13 +0400

    #day57 - new: jinja dev: APIs, templating, flask

---
+++ Day 57 - Blog_with_Jinja (Part 1)/main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from post import Post
+import requests
+
+posts = requests.get("https://api.npoint.io/5abcca6f4e39b4955965").json()  # temporary API
+post_objects = []
+for post in posts:
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_objects.append(post_obj)
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=post_objects)
+
+@app.route("/post/<int:idx>")
+def show_post(idx):
+    requested_post = None
+    for blog_post in post_objects:
+        if blog_post.id == idx:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+if __name__ == "__main__":
+    app.run(debug=True)
