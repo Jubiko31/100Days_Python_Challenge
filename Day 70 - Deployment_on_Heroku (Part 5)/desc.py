ccbf037e2d8bebdb1f5b0aa831a64df56d837dba
Jubiko31 <jubiko.macaberidze1@gmail.com>
2023-01-30 00:18:23 +0400

    #day70 - new: deployment, heroku, WSGI server, gunicorn, Procfile

---
+++ Day 70 - Deployment_on_Heroku (Part 5)/desc.py
@@ -0,0 +1,3 @@
+# On this chapter learnt how to deploy project on Heroku + WSGI Server with gunicorn
+# Need to create new file called "Procfile" in project folder to setup WSGI server with gunicorn
+# Heroku will call gunicorn to run our code and gunicorn will know how to speak to Heroku.
